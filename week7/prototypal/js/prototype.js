e6c2d5bc272ad0b2ade18899d68b66ce295eb59e
Steve Brownlee <chortlehoort@gmail.com>
2015-03-09 17:28:47 -0500

    Adding prototype code

--- week7/prototypal/js/prototype.js
+++ week7/prototypal/js/prototype.js
@@ -6,4 +6,27 @@ function Animal () {
 
 Animal();
 var salamander = new Animal();
+salamander.property = "slimy";
+
+var kangaroo = new Animal();
+kangaroo.property = "jumpy";
+
+console.log('salamander',salamander);
+console.log('kangaroo', kangaroo);
+
+function Doge () {
+  this.species = "Doge";
+}
+Doge.prototype = new Animal();
+
+var doge = new Doge();
+console.log('doge',doge);
+
+function Angus () {
+  this.name = "Angus";
+}
+Angus.prototype = new Doge();
+
+var angus = new Angus();
+console.log('angus',angus);
 
