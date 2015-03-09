83efdc0b1a44369463e42105fac8d64efaae98f4
Steve Brownlee <chortlehoort@gmail.com>
2015-03-09 17:20:05 -0500

    Adding closures code

--- week7/prototypal/js/prototype_assignment.js
+++ week7/prototypal/js/prototype_assignment.js
@@ -3,7 +3,6 @@
   1. Author functions that define the Make and Model of a Car and the Car itself.
   2. Create three instances of your Car that each have a different color.
   3. Each Car you create should inherit the make and model name.
-  4.
 */
 
 function Make () {
