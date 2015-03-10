4e85b311ae48f7fd5009c3344971b907ace0017e
Steve Brownlee <chortlehoort@gmail.com>
2015-03-09 22:31:39 -0500

    Updates and comments for the assignments

--- week7/prototypal/js/prototype_assignment.js
+++ week7/prototypal/js/prototype_assignment.js
@@ -1,16 +1,27 @@
 /*
   .....  YOUR MISSION  .....
-  1. Author functions that define the Make and Model of a Car and the Car itself.
-  2. Create three instances of your Car that each have a different color.
-  3. Each Car you create should inherit the make and model name.
+  1. Author functions that define the Make
+      and Model of a Car and the Car itself.
+  2. Create three instances of your Car that
+      each have a different color.
+  3. Each Car you create should inherit the
+      make and model name.
 */
 
 function Make () {
-  this.makeName = "";
+  this.makeName = "Ford";
 }
 
 function Model () {
-
+  this.modelName = "F-150";
 }
+// What is the prototype of a Model?
 
+// Define a Car
+// What is the prototype of a Car?
 
+// Create the first car
+
+// Create the second car
+
+// Create the third car
--- week7/scope/js/scope_assignment.js
+++ week7/scope/js/scope_assignment.js
@@ -5,21 +5,16 @@
 */
 
 
-function topTier (first) {
-
-  return function middleTier (second) {
-
-    return function bottomTier (third) {
-
-    }
-
-  }
-
-}
-
-var x = topTier("*");
-
 // This should be your output in the console
 //       *
 //      ***
 //     *****
+function topTier (first) {
+  return function middleTier (second) {
+    return function bottomTier (third) {
+      // Use your knowledge of lexical scope to output the variables here
+    }
+  }
+}
+
+var middle = topTier("*");
