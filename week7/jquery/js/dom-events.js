6233262ed34cdbf84cab127246d56bb80929d60a
Steve Brownlee <chortlehoort@gmail.com>
2015-03-14 10:30:53 -0500

    Updated jquery assignment

--- week7/jquery/js/dom-events.js
+++ week7/jquery/js/dom-events.js
@@ -1,3 +1,49 @@
 var targetDiv = document.getElementById("mousey");
 var hiddenDiv = document.getElementById("hidden");
 
+
+// Change the class when the mouse enters
+// targetDiv.onmouseover = function () {
+//   targetDiv.classList.remove('red');
+//   targetDiv.classList.add('blue');
+// }
+
+
+
+// You can toggle a class on an off every time you mouse over
+// targetDiv.onmouseover = function () {
+//   targetDiv.classList.toggle('red');
+// }
+
+
+
+// Turn the red off when mouse enters
+// Turn the red back on when the mouse leaves
+// targetDiv.onmouseover = function () {
+//   targetDiv.classList.remove('red');
+// }
+
+// targetDiv.onmouseout = function () {
+//   targetDiv.classList.add('red');
+// }
+
+
+
+// Why use JavaScript instead of CSS?
+// var peopleToDisplay = [];
+// peopleToDisplay.push("Robert Johnson");
+// peopleToDisplay.push("John Lee Hooker");
+// peopleToDisplay.push("T-Bone Walker");
+// peopleToDisplay.push("Muddy Waters");
+// peopleToDisplay.push("Big Mama Thornton");
+// peopleToDisplay.push("Robert Cray");
+
+// targetDiv.onmouseover = function () {
+//   hiddenDiv.classList.toggle('hide');
+//   hiddenDiv.innerHTML = peopleToDisplay.join("<br/>");
+// }
+
+// targetDiv.onmouseout = function () {
+//   hiddenDiv.classList.add('hide');
+// }
+
